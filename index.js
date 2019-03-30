5387410aa43e2d9e6f2f1d6983e740ad3364b8de
Joseph Reilly <reilly4181@gmail.com>
2019-03-30 21:41:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ return document.querySelector('div .target')
 }
 
 function deepestChild() {
-  var c = document.getElementById('#grand-node').children;
+  var c = document.getElementById('grand-node').children;
   var txt = "";
   var i;
   for (i = 0; i < c.length; i++) {
     txt = txt + c[i].tagName + "<br>";
   }
 
-  document.getElementById("demo").innerHTML = txt;
+  document.getElementById("grand-node").innerHTML = txt;
 }
