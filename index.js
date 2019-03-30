c448ceae8548540d626e5a9832212a7370a3e0bb
Joseph Reilly <reilly4181@gmail.com>
2019-03-30 21:46:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+  
+}
+
+function nestedTarget() {
+return document.querySelector('div .target')
+}
+
+function deepestChild() {
+    var cur = document.getElementById('grand-node')
+    var i = 0
+    while (cur.firstElementChild) {
+      i = i + 1
+      cur = cur.firstElementChild
+    }
+    return cur
+}
+
+function increaseRankBy(n) {
+    var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+    for (var i=0;i<lis.length;i++) {
+      lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+    }
+}
+
+
 
